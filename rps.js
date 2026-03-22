dafe3eed76666446d473e0b79b9270816230dc7e
Taj Tucker <tucker8258@gmail.com>
2026-03-22 01:07:49 -0500

    Fixed getHumanChoice function and Made playRound function

--- rps.js
+++ rps.js
@@ -1,24 +1,48 @@
 function getComputerChoice(){
-    const computerChoice = Math.floor(Math.random() * 3) + 1
-    if (computerChoice == 1){
+    const choice = Math.floor(Math.random() * 3) + 1;
+    if (choice == 1){
         return "rock"
-    } else if (computerChoice == 2){
+    } else if (choice == 2){
         return "paper"
     } else {
         return "scissors"
     }
-
 }
 
 function getHumanChoice(){
-    const humanChoice = prompt("Enter Rock, Paper, or Scissors: ")
-    if (humanChoice == "Rock"){
-        return "rock"
-    } else if (humanChoice == "Paper"){
-        return "paper"
-    } else if (humanChoice == "Scissors"){
-        return "scissors"
+    return prompt("Enter Rock, Paper, or Scissors: ")
+}
+
+let humanScore = 0
+let computerScore = 0
+
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase()
+    
+    if (humanChoice == "paper" && computerChoice == "rock"){ 
+        console.log("You Win! Paper beats Rock!")
+         humanScore += 1
+    } else if (humanChoice == "scissors" && computerChoice == "paper"){ 
+        console.log("You Win! Scissors beats Paper!")
+        humanScore += 1
+    } else if (humanChoice == "rock" && computerChoice == "scissors"){ 
+        console.log("You Win! Rock beats Scissors!")
+        humanScore += 1
+    } else if (humanChoice == "paper" && computerChoice == "scissors"){ 
+         console.log("You Lose! Scissors beats Paper!")
+         computerScore += 1
+    } else if (humanChoice == "scissors" && computerChoice == "rock"){ 
+         console.log("You Lose! Rock beats Scissors!")
+         computerScore += 1
+    } else if (humanChoice == "rock" && computerChoice == "paper"){ 
+         console.log("You Lose! Paper beats Rock!")
+         computerScore += 1
+    } else {
+        console.log("Tie")
     }
 }
 
-console.log(getHumanChoice())
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection)
