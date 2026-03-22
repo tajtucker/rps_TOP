27c0e4f6b6cbd9b01ce6f9e54756d52572102a27
Taj Tucker <tucker8258@gmail.com>
2026-03-22 00:32:18 -0500

    Complete Player Input for getHumanChoice Function

--- rps.js
+++ rps.js
@@ -1,12 +1,24 @@
 function getComputerChoice(){
-    const choice = Math.floor(Math.random() * 3) + 1;
-    if (choice == 1){
+    const computerChoice = Math.floor(Math.random() * 3) + 1
+    if (computerChoice == 1){
         return "rock"
-    } else if (choice == 2){
+    } else if (computerChoice == 2){
         return "paper"
     } else {
         return "scissors"
     }
 
 }
-console.log(getComputerChoice())
+
+function getHumanChoice(){
+    const humanChoice = prompt("Enter Rock, Paper, or Scissors: ")
+    if (humanChoice == "Rock"){
+        return "rock"
+    } else if (humanChoice == "Paper"){
+        return "paper"
+    } else if (humanChoice == "Scissors"){
+        return "scissors"
+    }
+}
+
+console.log(getHumanChoice())
